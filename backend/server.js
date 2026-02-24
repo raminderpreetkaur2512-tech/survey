5ae321b44838cc653a9abe473ea62090278d3c69
Raminderpreet Kaur <raminderpreetkaur2512@gmail.com>
2026-02-24 20:23:38 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 
 /* ==============================
@@ -55,6 +59,9 @@ const safeNumber = (val) => {
   return isNaN(num) ? null : num;
 };
 
+app.get("/", (req, res) => {
+  res.send("Survey Backend is Running 🚀");
+});
 
 // login api
 
@@ -286,7 +293,9 @@ app.get("/check-db", async (req, res) => {
    START SERVER
 ============================== */
 
-const PORT = 5000;
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
